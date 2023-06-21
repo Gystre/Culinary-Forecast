3494835fdf6b309c48f5dffe6dc92b43c37d8a6a
Kyle Yu <gystre@gmail.com>
2023-06-20 19:51:05 -0700

    edit metadata

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ export const Navbar: React.FC = () => {
   return (
     <nav className="flex justify-between">
       <div className="flex items-center">
-        <img src="/logo.webp" alt="logo" className="w-16 h-16" />
+        <img src="/logo.png" alt="logo" className="w-16 h-16" />
         <h1 className="text-h5 md:text-h4 font-bold ml-4">Culinary Forecast</h1>
       </div>
 
